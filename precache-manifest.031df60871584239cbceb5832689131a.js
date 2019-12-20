2966ef7510c132ccd09a551b73584cdb914a0429
bsixstyle <ihsan.bsix@gmail.com>
2019-12-20 13:56:08 +0700

    Updates

---
+++ precache-manifest.031df60871584239cbceb5832689131a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a626a9e56115019e16e9cf3806f268",
+    "url": "/scss/index.html"
+  },
+  {
+    "revision": "424a1b77cdbe9530841f",
+    "url": "/scss/static/css/main.b7646804.chunk.css"
+  },
+  {
+    "revision": "f603b3c082adf7eec7bf",
+    "url": "/scss/static/js/2.705ca177.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/scss/static/js/2.705ca177.chunk.js.LICENSE"
+  },
+  {
+    "revision": "424a1b77cdbe9530841f",
+    "url": "/scss/static/js/main.9f067eca.chunk.js"
+  },
+  {
+    "revision": "a2657188658b22d39023",
+    "url": "/scss/static/js/runtime-main.1bd14819.js"
+  }
+]);
